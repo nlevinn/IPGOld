a388d6df57d81d403647e1fc384798886d5163da
nlevinn <106302980+nlevinn@users.noreply.github.com>
2022-06-28 10:11:01 +0300

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -2,3 +2,17 @@ document.querySelector('.second-button').addEventListener('click', function () {
 
     document.querySelector('.animated-icon2').classList.toggle('open');
     });
+
+
+
+
+    var x = document.getElementById("contain");
+
+    x.addEventListener("click", myFunction);
+    
+    function myFunction() {
+      var element = document.getElementById("nav");
+      element.classList.toggle("open");
+      
+      x.classList.toggle("change");
+    }
